16f9595d210a4db16bbc3b5e341c198c5a3b522d
austenhasty <hastyae@gmail.com>
2019-07-22 10:54:28 -0700

    updated controller logic and json rendering

--- backend/study_buddy/app/controllers/api/v1/topics_controller.rb
+++ backend/study_buddy/app/controllers/api/v1/topics_controller.rb
@@ -1,2 +1,48 @@
 class Api::V1::TopicsController < ApplicationController
+  skip_before_action :authorized, only: [:create, :index, :update, :show, :destroy]
+
+  def index
+    topics = Topic.all
+    render json: topics
+  end
+
+  def show
+    topic = Topic.find_by(id: params[:id])
+    render json: TopicSerializer.new(topic)
+  end
+
+  def create
+    topic = Topic.new(topic_params)
+    if topic.save
+      render json: {topic: TopicSerializer.new(topic) }, status: :created
+    else
+      render json: {error: 'failed to create topic'}, status: :not_acceptable
+    end
+  end
+
+  def update
+    topic = Topic.find_by(id: params[:id])
+    if topic.update(topic_params)
+      render json: TopicSerializer.new(topic)
+    else
+      puts error.full_message
+    end
+  end
+
+
+  def destroy
+    topic = Topic.find_by(id: params[:id])
+    if topic.destroy
+      render json: TopicSerializer.new(topic)
+    else
+      puts error.full_message
+    end
+
+  end
+
+  private
+
+  def topic_params
+    params.require(:topic).permit(:name)
+  end
 end
--- backend/study_buddy/app/controllers/api/v1/users_controller.rb
+++ backend/study_buddy/app/controllers/api/v1/users_controller.rb
@@ -2,9 +2,23 @@ class Api::V1::UsersController < ApplicationController
   skip_before_action :authorized
 
   def index
-    @users = User.all
-    render json: @users
+    users = User.all
+    render json: users
   end
+
+  def create
+    @user = User.create(user_params)
+    if @user.valid?
+      render json: { user: UserSerializer.new(@user) }, status: :created
+    else
+      render json: { error: 'failed to create user' }, status: :not_acceptable
+    end
+  end
+
+  def show
+       user = User.find_by(id: params[:id])
+       render json: UserSerializer.new(user)
+   end
 
 
 
