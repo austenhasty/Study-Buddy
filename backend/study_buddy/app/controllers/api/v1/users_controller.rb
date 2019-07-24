fd100160957792ea16abfcb46c6a5245f6e3d222
austenhasty <hastyae@gmail.com>
2019-07-23 17:16:57 -0700

    nested params issue

--- backend/study_buddy/app/controllers/api/v1/users_controller.rb
+++ backend/study_buddy/app/controllers/api/v1/users_controller.rb
@@ -12,6 +12,7 @@ class Api::V1::UsersController < ApplicationController
 
   def create
     @user = User.create(user_params)
+    # byebug
     if @user.valid?
       render json: { user: UserSerializer.new(@user) }, status: :created
     else
