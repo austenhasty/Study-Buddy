9600f708c5411e51847f29cb1654c671a86916f1
austenhasty <hastyae@gmail.com>
2019-07-24 11:00:02 -0700

    problems getting notecard info

--- backend/study_buddy/app/serializers/topic_serializer.rb
+++ backend/study_buddy/app/serializers/topic_serializer.rb
@@ -1,7 +1,7 @@
 class TopicSerializer < ActiveModel::Serializer
   attributes :id, :name
-  has_many :users, through: :users_topics
-  has_many :users_topics
-  has_many :notecards, through: :topics_notecards
-  has_many :topics_notecards
+  # has_many :users_topics
+  # has_many :users, through: :users_topics
+  # has_many :topics_notecards
+  # has_many :notecards, through: :topics_notecards
 end
