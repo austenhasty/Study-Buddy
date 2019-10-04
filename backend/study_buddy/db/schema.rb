515e9cd4fff829f9ce3964ce224f982730739a6b
austenhasty <hastyae@gmail.com>
2019-10-04 16:49:01 -0700

    seeding new data and still playing with relationships

--- backend/study_buddy/db/schema.rb
+++ backend/study_buddy/db/schema.rb
@@ -16,9 +16,9 @@ ActiveRecord::Schema.define(version: 2019_07_18_215619) do
   enable_extension "plpgsql"
 
   create_table "notecards", force: :cascade do |t|
-    t.integer "topic_id"
     t.string "term"
     t.string "definition"
+    t.integer "topic_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
